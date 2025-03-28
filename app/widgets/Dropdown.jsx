5c4a9a0dae54f4f2fe51d1c8a1440bcf1334c694
kaveeshala <it22110916@my.sliit.lk>
2025-03-28 19:42:28 +0530

    change the language in dropdown menu

--- app/widgets/Dropdown.jsx
+++ app/widgets/Dropdown.jsx
@@ -11,23 +11,34 @@ import {
 
 export default function Dropdown() {
     //const [selectedLanguage, setSelectedLanguage] = useState("English");
-    const [isOpen, setIsOpen] = useState(false);
+    //const [isOpen, setIsOpen] = useState(false);
     const {LangState, setLangState}   = useLangState();
+
+    const languages = {
+      English: "English",
+      Japanese: "Japanese",
+      Russian: "Russian",
+    }
 
     const handleLanguageChange = (language) => {
         setLangState(language);
-        setIsOpen(false);
+        //setIsOpen(false);
     }
-  return (
-  
-       <DropdownMenu>
-                <DropdownMenuTrigger>Language</DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuItem onClick={() => handleLanguageChange("English")}>Engilsh</DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => handleLanguageChange("Japaneese")}>Japaneese</DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => handleLanguageChange("Russian")}>Russian</DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-   
-  )
+    return (
+      <DropdownMenu>
+          <DropdownMenuTrigger className="px-4 py-2">
+              {languages[LangState] || "Language"}
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+              {Object.entries(languages).map(([key, value]) => (
+                  <DropdownMenuItem 
+                      key={key}
+                      onClick={() => handleLanguageChange(key)}
+                  >
+                      {value}
+                  </DropdownMenuItem>
+              ))}
+          </DropdownMenuContent>
+      </DropdownMenu>
+  );
 }
